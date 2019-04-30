673944d58147076a0ed0ad92a6699d0479ac3a0f
z-yonghong <3230744579@qq.com>
2019-04-30 20:56:19 +0800

    4.28 周日 第二次

--- js/huawei.js
+++ js/huawei.js
@@ -1,10 +1,6 @@
 window.onscroll=function () {
     var Old = document.documentElement.scrollTop;
     var Top=document.getElementById("Top");
-    // var Top02=document.getElementById("Top02");
-    // if(Old<20){
-    //     Top.style.top=0+"px";
-    // };
     if(Old>20){
         Top.style.top=-28+"px";
     };
@@ -17,15 +13,9 @@ window.onscroll=function () {
         New = document.documentElement.scrollTop;
         if(Old>New){
             Top.style.top=-28+"px";
-            // console.log('Old:'+Old);
-            // console.log('New:'+New);
-            // console.log('shanghua');
         };
         if(Old<New){
             Top.style.top=-102+"px";
-            // console.log('Old:'+Old);
-            // console.log('New:'+New);
-            // console.log('xiahua');
         };
         if(Old<20){
             Top.style.top=0+"px";
